710d719c95c2a98111a3fbbb73836ee37d3db2c3
nghia <huunghia.nguyen@dssolution.jp>
2023-10-05 00:10:15 +0700

    update search word

--- src/modules/Viet_tay/viTay.controller.js
+++ src/modules/Viet_tay/viTay.controller.js
@@ -47,11 +47,22 @@ const translateTayToViet = async (req, res) => {
 
 const translateVietToTay = async (req, res) => {
     const query = req.query;
-    console.log(33333);
     console.log(query.query);
-    console.log(1111);
     try {
-        return await viTayService.translate(query.query, 'viet');
+        const word = await viTayService.translate(query.query, 'viet');
+        console.log('word', word);
+        if (word === query.query) {
+            return res.status(400).send({
+                data: 'không lấy được data',
+                success: false,
+                message: 'Lấy data thất bại',
+            });
+        }
+        return res.status(200).send({
+            data: word,
+            success: true,
+            message: 'Lấy data thành công',
+        });
     } catch (err) {
         throw err;
     }
